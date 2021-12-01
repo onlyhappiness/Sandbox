fb8fa5c23d38100b619d989f3921495ff9064bec
onlyhappiness96 <onlyhappiness96@gmail.com>
2021-12-01 21:02:48 +0900

    이렇게 해봥

---
+++ 20211201/test/src/data/data.js
@@ -0,0 +1,150 @@
+let data = {
+	"type": {
+		"chucheon": "",
+		"id": "고유번호",
+		"category": "카테고리",
+		"name": "트레이너명",
+		"center": "센터명",
+		"area": "지역",
+		"progress": "온라인 PT 세션 현황"
+	},
+	"session":  {
+		"order": 'NO',
+		"member": "회원",
+		"session": "세션",
+		"progress": "진행여부"
+	},
+	"member": [
+		{
+			"thumbs": "추천",
+			"id": "1",
+			"category": "헬스",
+			"name": "홍길동",
+			"center": "ABC 헬스장",
+			"area": "서울 마포구",
+			"session": "",
+			"progress": [
+				{
+					"id": "1",
+					"name": "트와이스",
+					"session": "1",
+					"progress": "시작 전"
+				},
+				{
+					"id": "2",
+					"name": "고길동",
+					"session": "3",
+					"progress": "진행중"
+				},
+				{
+					"id": "3",
+					"name": "블라블라",
+					"session": "4",
+					"progress": "완료"
+				},
+				{
+					"id": "4",
+					"name": "여자친구",
+					"session": "5",
+					"progress": "진행중"
+				},
+			]
+		},
+		{
+			"thumbs": "추천",
+			"id": "2",
+			"category": "개인",
+			"name": "도우너",
+			"center": "센터",
+			"area": "서울 마포구",
+			"session": "",
+			"progress": [
+				{
+					"id": "1",
+					"name": "엉엉",
+					"session": "3",
+					"progress": "진행중"
+				},
+				{
+					"id": "2",
+					"name": "블라블라",
+					"session": "3",
+					"progress": "진행중"
+				},
+				{
+					"id": "3",
+					"name": "올?",
+					"session": "2",
+					"progress": "완료"
+				},
+				
+			]
+		},
+		{
+			"thumbs": "추천",
+			"id": "3",
+			"category": "개인",
+			"name": "도우너",
+			"center": "소속이",
+			"area": "서울 마포구",
+			"session": "",
+			"progress": [
+				{
+					"id": "1",
+					"name": "asap",
+					"session": "1",
+					"progress": "진행중"
+				},
+				{
+					"id": "2",
+					"name": "제발",
+					"session": "3",
+					"progress": "진행중"
+				},
+				{
+					"id": "3",
+					"name": "됐으면 좋겠어요",
+					"session": "4",
+					"progress": "완료"
+				},
+			]
+		},
+		{
+			"thumbs": "",
+			"id": "4",
+			"category": "헬스",
+			"name": "홍길동",
+			"center": "안 된 트레이너는 ",
+			"area": "서울 마포구",
+			"session": "",
+			"progress": [
+				{
+					"id": "1",
+					"name": "트와이스",
+					"session": "1",
+					"progress": "시작 전"
+				},
+				{
+					"id": "2",
+					"name": "고길동",
+					"session": "3",
+					"progress": "진행중"
+				},
+			]
+		},
+		{
+			"thumbs": "",
+			"id": "5",
+			"category": "헬스",
+			"name": "홍길동",
+			"center": "없습니다",
+			"area": "서울 마포구",
+			"session": "",
+			"progress": [
+				
+			]
+		},
+	]
+}
+
+export default data;
