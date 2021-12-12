9d6135956e27d6892fffed06f57bf9daa2bda330
onlyhappiness96 <onlyhappiness96@gmail.com>
2021-12-12 22:42:29 +0900

    Fix folder cleanup

---
+++ mui/README.md
@@ -0,0 +1,4 @@
+## 참고
+https://mui.com/components/data-grid/
+
+https://mui.com/components/data-grid/getting-started/#installation
---
+++ mui/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TableDemo from './components/Data-Grid/TableDemo'
+
+function App() {
+  return (
+    <div>
+      <TableDemo />
+    </div>
+  )
+}
+
+export default App
